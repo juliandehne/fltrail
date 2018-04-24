5262a4ecc114c6ce98392ed487c2a507f515142a
Julian Dehne <julian.dehne@uni-potsdam.de>
2018-04-24 13:15:38 +0200

    first commit for demo in class

---
+++ gemeinsamforschen/src/scripts/munschkins.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS Munschkins (
+  MunschkinId int,
+  LastName varchar(255),
+  FirstName varchar(255),
+  BadThings varchar(255),
+  Strength int
+);
+
+INSERT INTO `Munschkins` (MunschkinId, LastName, FirstName, BadThings, Strength) values(1, "Thor", "Theodor", "he
+will eat your feet", 5);
