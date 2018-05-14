9b47a1b2638e146cf40c921d2cf7ccf8d6826fff
Axel <wiepke@uni-potsdam.de>
2018-05-14 07:34:13 +0200

    createPreferences uses getUserTokenFromUrl

--- studentview/assets/js/createPreferences.js
+++ studentview/assets/js/createPreferences.js
@@ -161,14 +161,7 @@ function takesPartInProject() {
             console.log(response);
             deblockScreen();
             writeTime();
-
-            var parts = window.location.search.substr(1).split("&");
-            var $_GET = {};
-            for (var i = 0; i < parts.length; i++) {
-                var temp = parts[i].split("=");
-                $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
-            }
-            location.href = "projects.php?token=" + $_GET['token'] + "&timeout=false";
+            location.href = "projects.php?token=" +getUserTokenFromUrl() + "&timeout=false";
         },
         error: function (a, b, c) {
             console.log(a);
