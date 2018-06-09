a0513fd29cba8f86423c4cd0da210c2c4d676968
tudtianus <tudtianus@gmail.com>
2018-06-09 10:11:58 +0200

    fix: Updated Journal Interface

--- gemeinsamforschen/src/main/java/unipotsdam/gf/interfaces/IJournal.java
+++ gemeinsamforschen/src/main/java/unipotsdam/gf/interfaces/IJournal.java
@@ -1,63 +1,18 @@
 package unipotsdam.gf.interfaces;
 
-import unipotsdam.gf.modules.journal.JournalEntry;
+import unipotsdam.gf.modules.assessment.controller.StudentIdentifier;
 
 /**
- * Interface for learning journal entry
+ * Interface for learning journal
  */
+
 public interface IJournal {
 
     /**
-     * Enum for visibility
+     * Exports the learning journal
+     * @param student StudentIdentifier
+     * @return the journal as String (may change)
      */
-    enum Visibility{ALL, STUDENT, DOZENT, NONE}
-
-
-    /**
-     * Add a new journal entry
-     *
-     * @param text text of the entry
-     * @param visibility visibility of the entry
-     */
-    void addJournal(String text, Visibility visibility);
-
-    /**
-     * Change an existing journal entry
-     *
-      * @param newText the new text
-     */
-    void editJournal(String newText);
-
-    /**
-     * Delete a journal entry
-     *
-     * @param journaId id of the entry
-     */
-    void deleteJournal(long journaId);
-
-    /**
-     * change visibility of an entry
-     * @param journaId id of the entry
-     * @param visibility new visibility
-     */
-    void setVisibility(long journaId, Visibility visibility);
-
-    /**
-
-     * Get specific journal entry
-     *
-     * @param journaId id of the entry
-     * @return JournalEntry from database
-     */
-    JournalEntry getJournal(long journaId);
-
-    /**
-     * Get all JournalEntry for a project
-     *
-     * @param projectId id of project
-     * @return all JournalEnrys for that project
-     */
-    JournalEntry getAllJournalEntrysProject(long projectId);
-
+    String exportJournal (StudentIdentifier student);
 
 }
