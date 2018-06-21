318f992fc84d3be768e7830a3bd2ac0ea2608059
Sven Kästle <sven.kaestle@gmx.de>
2018-06-21 16:38:44 +0200

    feat: hovering over an annotation will highlight the corresponding text

--- gemeinsamforschen/src/main/webapp/assets/js/annotationScript.js
+++ gemeinsamforschen/src/main/webapp/assets/js/annotationScript.js
@@ -1,67 +1,203 @@
+// set color, userId and targetId
+var userId = 11;
+var targetId = 200;
+var color = getRandomColor();
+
+// declare document text
+var documentText;
+
+/**
+ * This function will fire when the DOM is ready
+ */
 $(document).ready(function() {
 
-    console.log("start ready function");
-
+    /**
+     * Context menu handler
+     */
     $.contextMenu({
         selector: '.context-menu-one',
         callback: function(key, options) {
 
-            console.log("start annotation");
-
-            var text = generateId(6);
-            var user = generateId(1).toUpperCase();
-            var color = getRandomColor();
-
-            $.fn.addAnnotation(user, text, color);
-
+            // close context menu
             window.close;
+
+            // initialize selected body
+            var body = getSelectedText();
+
+            // if user selected something
+            if (body.length > 0) {
+                // annotationPostRequest
+                var request = {
+                    userId: userId,
+                    targetId: targetId,
+                    body: body,
+                    startCharacter: window.getSelection().getRangeAt(0).startOffset,
+                    endCharacter: window.getSelection().getRangeAt(0).endOffset
+                };
+
+                console.log(request);
+
+                createAnnotation(request, function(response) {
+                    // display the new annotation
+                    displayAnnotation(response);
+
+                });
+            }
+
         },
         items: {
             "annotation": {name: "Annotation", icon: "edit"}
         }
     });
+    /*
+     *   PAGE LOADED
+     */
+    documentText = $('#documentText').html();
 
-    $('.context-menu-one').on('click', function(e){
-        console.log('clicked', this);
-    })
+    // fetch annotations from server on page start
+    getAnnotations(targetId, function (response) {
+        // iterate over annotations and display each
+        $.each(response, function (i, annotation) {
+            displayAnnotation(annotation);
+        })
+    });
 
-    $.fn.addAnnotation = function(title, text, color) {
-        var list = $('#annotations')
-        var selection = getSelectedText()
-
-        if (selection.length > 0) {
-
-            var replacement = $('<span></span>').css('background-color', color).html(selection);
-
-            var replacementHtml = $('<div>').append(replacement.clone()).remove().html();
-
-            $('#hiho').html( $('#hiho').html().replace(selection, replacementHtml) );
-
-            if ($('#annotations li').filter( ".listelement" ).length > 0) {
-                list.prepend(
-                    $('<li>').attr('class', 'spacing')
-                )
-            }
-
-            list.prepend(
-                $('<li>').attr('class', 'listelement').append(
-                    $('<div>').attr('class', 'card').append(
-                        $('<div>').attr('class', 'cardAvatar').css('background-color', color).append(
-                            $('<b>').append(title)
-                        )
-                    ).append(
-                        $('<div>').attr('class', 'cardContent').append(
-                            $('<span>').append(selection.substring(0, 5) + "...")
-                        )
-                    )
-                )
-            );
-        }
-    }
-
-    console.log("end ready function");
 });
 
+/**
+ * POST: Save an annotation in the database
+ *
+ * @param annotationPostRequest
+ * @param responseHandler
+ */
+function createAnnotation(annotationPostRequest, responseHandler) {
+    var url = "http://localhost:8080/rest/annotations/";
+    var json = JSON.stringify(annotationPostRequest);
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: json,
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            responseHandler(response);
+        }
+    });
+}
+
+/**
+ * PATCH: Alter an annotation
+ *
+ * @param annotationPatchRequest
+ * @param responseHandler
+ */
+function alterAnnotation(id, annotationPatchRequest, responseHandler) {
+    var url = "http://localhost:8080/rest/annotations/" + id;
+    var json = JSON.stringify(annotationPatchRequest);
+    $.ajax({
+        url: url,
+        type: "PATCH",
+        data: json,
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            responseHandler(response);
+        }
+    });
+}
+
+/**
+ * GET: Get all annotations for a specific target
+ *
+ *
+ * @param targetId
+ * @param responseHandler
+ */
+function getAnnotations(targetId, responseHandler) {
+    var url = "http://localhost:8080/rest/annotations/target/" + targetId;
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            // sort the responding annotations by timestamp (DESC)
+            response.sort(function (a, b) {
+                return a.timestamp - b.timestamp;
+            });
+            // handle the response
+            responseHandler(response);
+        }
+    });
+}
+
+/**
+ * Display annotation in the list
+ *
+ * @param annotation the annotation to be displayed
+ */
+function displayAnnotation(annotation) {
+    // fetch list of annotations
+    var list = $('#annotations')
+
+    // insert spacing if we need one
+    if ($('#annotations li').filter( ".listelement" ).length > 0) {
+        list.prepend(
+            $('<li>').attr('class', 'spacing')
+        )
+    }
+
+    // insert annotation card
+    list.prepend(
+        $('<li>').mouseenter(function () {
+            addHighlightedText(annotation.startCharacter, annotation.endCharacter);
+        }).mouseleave(function () {
+            deleteHighlightedText();
+        }).data(annotation).attr('class', 'listelement').attr('id', annotation.id).append(
+            $('<div>').attr('class', 'card').append(
+                $('<div>').attr('class', 'cardAvatar').css('background-color', color).append(
+                    $('<b>').append(annotation.id.substr(0,1))
+                )
+            ).append(
+                $('<div>').attr('class', 'cardContent').append(
+                    $('<span>').append(annotation.body.substring(0, 5) + "...")
+                )
+            )
+        )
+    );
+}
+
+/**
+ * Add a highlighted text at specific position
+ *
+ * @param startCharacter the offset of the start character
+ * @param endCharacter the offset of the end character
+ */
+function addHighlightedText(startCharacter, endCharacter) {
+    // create <span> tag with the annotated text
+    var replacement = $('<span></span>').css('background-color', color).html(documentText.slice(startCharacter, endCharacter));
+
+    // wrap an <p> tag around the replacement, get its parent (the <p>) and ask for the html
+    var replacementHtml = replacement.wrap('<p/>').parent().html();
+
+    // insert the replacementHtml
+    var newDocument = documentText.slice(0, startCharacter) + replacementHtml + documentText.slice(endCharacter);
+
+    // set new document text
+    $('#documentText').html(newDocument);
+}
+
+/**
+ * Restore the base text
+ */
+function deleteHighlightedText() {
+    $('#documentText').html(documentText);
+}
+
+/**
+ * Get the text value of the selected text
+ *
+ * @returns {*} the text
+ */
 function getSelectedText() {
     if(window.getSelection){
         return window.getSelection().toString();
@@ -74,16 +210,11 @@ function getSelectedText() {
     }
 }
 
-function generateId(length) {
-    var id = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-    for (var i = 0; i < length; i++) {
-        id += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return id;
-}
-
+/**
+ * Generate a random color of the format 'rgb(r, g, b)'
+ *
+ * @returns {string} the new color
+ */
 function getRandomColor() {
     return 'rgb(' +
         (Math.floor(Math.random()*56)+170) + ', ' +
