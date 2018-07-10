6209292822fde1d711e2b42375a73744c41e9f60
Julian Dehne <julian.dehne@uni-potsdam.de>
2018-07-10 12:33:41 +0200

    added rest service for phases

--- gemeinsamforschen/src/main/java/unipotsdam/gf/core/states/ProjectPhase.java
+++ gemeinsamforschen/src/main/java/unipotsdam/gf/core/states/ProjectPhase.java
@@ -2,5 +2,5 @@ package unipotsdam.gf.core.states;
 
 public enum ProjectPhase {
     CourseCreation, GroupFormation, DossierFeedback, Execution, Assessment,
-    Projectfinished
+    Projectfinished;
 }
