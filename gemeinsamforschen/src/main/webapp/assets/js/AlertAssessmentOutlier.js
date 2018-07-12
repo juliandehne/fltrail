b30f272dc22a261a0148e1b14c5152f18ebc8797
Christian <chgottsc@uni-potsdam.de>
2018-07-12 11:16:47 +0200

    feat. project-docent_CG mit eingabemöglichkeit der Noten vom Dozenten, angepasster Link-Pfad

--- gemeinsamforschen/src/main/webapp/assets/js/AlertAssessmentOutlier.js
+++ gemeinsamforschen/src/main/webapp/assets/js/AlertAssessmentOutlier.js
@@ -1,7 +1,201 @@
 $(document).ready(function () {
+    if(!$("#Diagramm").hidden){
+        $("#Diagramm").hide();
+    };
+
+    document.getElementById("Diagramm").style.visibility="invisible";
+    $('#DiaBlende').on('click',function () {
+        $("#Diagramm").show();
+        document.getElementById("demo").innerHTML="verlauf";
+    });
+});
+$(document).ready(function () {
     $('#ProblemGrp1').on('click',function () {
         var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
-        document.getElementById("demo").innerHTML = "Wurde übernommen";
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
     });
 });
 
+$(document).ready(function () {
+    $('#ProblemGrp1S1').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
+$(document).ready(function () {
+    $('#ProblemGrp1S2').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
+$(document).ready(function () {
+    $('#ProblemGrp2').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
+$(document).ready(function () {
+    $('#ProblemGrp2S3').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
+$(document).ready(function () {
+    $('#ProblemGrp2S4').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
+$(document).ready(function () {
+    $('#ProblemGrp3').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
+$(document).ready(function () {
+    $('#ProblemGrp3S5').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
+$(document).ready(function () {
+    $('#ProblemGrp3S6').on('click',function () {
+        var alarm = prompt("hier gibt es Unstimmigeiten","Vorgeschlagene Note:")
+        if (alarm === "1") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "2") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "3") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "4") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "5") {
+            document.getElementById("demo").innerHTML = alarm;
+        } else if (alarm === "6") {
+            document.getElementById("demo").innerHTML = alarm;
+        }
+        else{
+            window.alert("Bitte eine Zahl zwischen eins und 6 angeben")
+        }
+    });
+});
--- gemeinsamforschen/src/main/webapp/assets/js/overview-docent.js
+++ gemeinsamforschen/src/main/webapp/assets/js/overview-docent.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('#project1Link').on('click', function(){
-        location.href="project-docent.jsp?token="+getUserTokenFromUrl();
+        location.href="project-docent_CG.jsp?token="+getUserTokenFromUrl();
     });
     $('#project2Link').on('click', function(){
-        location.href="project-docent.jsp?token="+getUserTokenFromUrl();
+        location.href="project-docent_CG.jsp?token="+getUserTokenFromUrl();
     });
     $('#createProject').on('click', function(){
         location.href="createProject.jsp?token="+getUserTokenFromUrl();
