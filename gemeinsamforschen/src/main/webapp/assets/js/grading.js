61926e1357fbb2fe0eae6199d6897ecf86ba5357
Christian <chgottsc@uni-potsdam.de>
2018-07-18 20:20:13 +0200

    feat. grading.js eingebunden um noten zu finalisieren

---
+++ gemeinsamforschen/src/main/webapp/assets/js/grading.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#ProjektFinalisieren').on('click',function () {
+        //test um ansprechen des skriptes zu garantieren
+        document.getElementById("ProjektFinalisieren").style.color="magenta"
+
+    });
+});
