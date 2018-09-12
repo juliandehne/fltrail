206beb7e3be0c02e61710d7574a0c9fcc9c92230
Johannes Zeiße <johannes.zeisse@uni-potsdam.de>
2018-09-12 11:40:25 +0200

    Updated

--- gemeinsamforschen/src/scripts/dbschema/researchReport.sql
+++ gemeinsamforschen/src/scripts/dbschema/researchReport.sql
@@ -1,16 +1,17 @@
-CREATE TABLE if not exists researchReport (
-   `id` varchar(400) NOT NULL,
-   `author` varchar(400) NOT NULL,
-   `title` varchar(400) NOT NULL,
-   `method` varchar(10000) NOT NULL,
-   `research` varchar(10000) NOT NULL,
-   `question` varchar(10000) NOT NULL,
-   `concept` varchar(10000) NOT NULL,
-   `done` varchar(10000) NOT NULL,
-   `evaluation` varchar(10000) NOT NULL,
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `researchReport` (
+  `reportID` varchar(400) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `title` varchar(400) NOT NULL,
+  `method` text NOT NULL,
+  `research` text NOT NULL,
+  `question` text NOT NULL,
+  `concept` text NOT NULL,
+  `do` text NOT NULL,
+  `evaluation` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE if not exists biblography (
-   `id` varchar(400) NOT NULL,
-   `source` varchar(400) NOT NULL,
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `biblography` (
+  `reportID` varchar(400) NOT NULL,
+  `source` varchar(400) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
