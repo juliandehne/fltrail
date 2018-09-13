24a583b504f5d562a5ce4592c30a9fa7af3eca31
Axel <wiepke@uni-potsdam.de>
2018-09-13 09:35:57 +0200

    delete unnesscary file

--- gemeinsamforschen/src/main/java/unipotsdam/gf/core/management/pageAppearance/OmniDependencies.java
+++ gemeinsamforschen/src/main/java/unipotsdam/gf/core/management/pageAppearance/OmniDependencies.java
@@ -21,7 +21,6 @@ public class OmniDependencies extends SimpleTagSupport {
                 "    <link rel=\"stylesheet\" href=\""+hierarchyToString(hierarchyLevel)+"libs/fonts/font-awesome.min.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\""+hierarchyToString(hierarchyLevel)+"libs/css/Sidebar-Menu-1.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\""+hierarchyToString(hierarchyLevel)+"libs/css/Sidebar-Menu.css\">\n" +
-                "    <script src=\""+hierarchyToString(hierarchyLevel)+"libs/js/Sidebar-Menu.js\"></script>\n" +
                 "    <script src=\""+hierarchyToString(hierarchyLevel)+"core/utility.js\"></script>\n" +
                 "    <script src=\""+hierarchyToString(hierarchyLevel)+"core/footer.js\"></script>\n" +
                 "    <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.1.0/css/all.css\"\n" +
