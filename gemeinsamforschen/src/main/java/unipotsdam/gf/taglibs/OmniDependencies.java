98fe342026aa6b013d64006ce7dd7d3f9b639f6d
Maren <maren.schulze@uni-potsdam.de>
2018-11-30 16:12:24 +0100

    delted font awesome pathes and doublicate date

--- gemeinsamforschen/src/main/java/unipotsdam/gf/taglibs/OmniDependencies.java
+++ gemeinsamforschen/src/main/java/unipotsdam/gf/taglibs/OmniDependencies.java
@@ -18,22 +18,23 @@ public class OmniDependencies extends SimpleTagSupport {
                 "    <link rel=\"stylesheet\" href=\""+hierarchyToString(hierarchyLevel)+"libs/bootstrap/css/bootstrap3.3.7.min.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/css/styles.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "taglibs/css/footer.css\">\n" +
+                "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "taglibs/css/css-fa/all.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "taglibs/css/global.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "taglibs/css/normalize.css\">\n" +
-                "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "taglibs/css/css-fa/all.css\" integrity=\"sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU\" crossorigin=\"anonymous\">\n" +
+
                 "    <script src=\"" + hierarchyToString(hierarchyLevel) + "libs/jquery/jquery.3.3.1.min.js\"></script>\n" +
                 "    <script src=\"" + hierarchyToString(hierarchyLevel) + "libs/jquery/jquery.3.3.7.min.js\"></script>\n" +
-                "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/fonts/font-awesome.min.css\">\n" +
+               // "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/fonts/font-awesome.min.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/css/Sidebar-Menu-1.css\">\n" +
                 "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/css/Sidebar-Menu.css\">\n" +
                 "    <script src=\"" + hierarchyToString(hierarchyLevel) + "taglibs/js/utility.js\"></script>\n" +
                 "    <script src=\"" + hierarchyToString(hierarchyLevel) + "taglibs/js/footer.js\"></script>\n" +
-                "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "project/css/all.css\">" +
-                "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "project/css/style.css\" type=\"text/css\">" +
+              //  "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "project/css/all.css\">" +
+              //  "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "project/css/style.css\" type=\"text/css\">" +
                 "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/tagsinput/jquery.tagsinput.min.css\">\n" +
                 "    <script src=\"" + hierarchyToString(hierarchyLevel) + "libs/tagsinput/jquery.tagsinput.min.js\"></script>\n" +
                 "    <script type=\"text/javascript\" src=\"" + hierarchyToString(hierarchyLevel) + "libs/jquery/jqueryTemplate.js\"></script>"+
-                "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/fonts/fontawesome.5.1.0.css/\"\n" +
+               // "    <link rel=\"stylesheet\" href=\"" + hierarchyToString(hierarchyLevel) + "libs/fonts/fontawesome.5.1.0.css/\"\n" +
                 "      integrity=\"sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt\" crossorigin=\"anonymous\">");
     }
 
