2df4cd7068eb4506b3a7b0d58493771eda07e488
Martin Staehr <software@mstaehr.net>
2019-03-31 12:28:22 +0200

    add todos for rebuild of annotation module with quillJs

--- gemeinsamforschen/src/main/webapp/annotation/js/unstructuredAnnotation.js
+++ gemeinsamforschen/src/main/webapp/annotation/js/unstructuredAnnotation.js
@@ -79,9 +79,12 @@ $(document).ready(function () {
  */
 function handleCategorySelection(category, color, startCharacter, endCharacter) {
     // if highlighting is possible
+    // TODO: reimplement isAlreadyHighlighted for quillJs
     if (!isAlreadyHighlighted(startCharacter, endCharacter)) {
         // TODO: change to quill subject
-        // check if element has 'not-added' class
+        // TODO: add status bar switch if text is annotated
+        // TODO: add save for backend
+        // TODO: add reload of saved annotations after site reload
         let length = endCharacter - startCharacter;
         quill.formatText(startCharacter,length, 'background', color);
 
