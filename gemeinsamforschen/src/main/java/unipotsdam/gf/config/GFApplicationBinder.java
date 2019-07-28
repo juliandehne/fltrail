016fac68f4afd779d500d7614eb79791d91088a2
Martin Staehr <software@mstaehr.net>
2019-07-28 12:33:58 +0200

    #174 #180 bind DummyExecutionProcess until executionphase is implemented in wizard

--- gemeinsamforschen/src/main/java/unipotsdam/gf/config/GFApplicationBinder.java
+++ gemeinsamforschen/src/main/java/unipotsdam/gf/config/GFApplicationBinder.java
@@ -55,6 +55,7 @@ import unipotsdam.gf.modules.wizard.WizardDao;
 import unipotsdam.gf.mysql.MysqlConnect;
 import unipotsdam.gf.mysql.MysqlConnectImpl;
 import unipotsdam.gf.process.DossierCreationProcess;
+import unipotsdam.gf.process.DummyExecutionProcess;
 import unipotsdam.gf.process.ExecutionProcess;
 import unipotsdam.gf.process.GroupFormationProcess;
 import unipotsdam.gf.process.IExecutionProcess;
@@ -62,7 +63,6 @@ import unipotsdam.gf.process.PeerAssessmentProcess;
 import unipotsdam.gf.process.PortfolioProcess;
 import unipotsdam.gf.process.ProjectCreationProcess;
 import unipotsdam.gf.process.SurveyProcess;
-import unipotsdam.gf.process.*;
 import unipotsdam.gf.process.constraints.ConstraintsImpl;
 import unipotsdam.gf.process.phases.PhasesImpl;
 import unipotsdam.gf.process.tasks.Task;
@@ -141,8 +141,8 @@ public class GFApplicationBinder extends AbstractBinder {
         /*
          * TODO: @Martin comment in for your development
          */
-        //bind(DummyExecutionProcess.class).to(IExecutionProcess.class);
-        bind(ExecutionProcess.class).to(IExecutionProcess.class);
+        bind(DummyExecutionProcess.class).to(IExecutionProcess.class);
+        //bind(ExecutionProcess.class).to(IExecutionProcess.class);
         bind(PortfolioService.class).to(IPortfolioService.class);
         bind(ReflectionQuestionDAO.class).to(ReflectionQuestionDAO.class);
         bind(ReflectionQuestionsStoreDAO.class).to(ReflectionQuestionsStoreDAO.class);
