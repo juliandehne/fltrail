7504e8574970378a5e0fe5c3231ff0590aed47ff
Julian Dehne <julian.dehne@uni-potsdam.de>
2019-08-09 13:54:25 +0200

    started implementing reflectionwizard randomizer

--- gemeinsamforschen/src/main/java/unipotsdam/gf/modules/wizard/ReflectionPhaseSimulation.java
+++ gemeinsamforschen/src/main/java/unipotsdam/gf/modules/wizard/ReflectionPhaseSimulation.java
@@ -1,10 +1,13 @@
 package unipotsdam.gf.modules.wizard;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import de.svenjacobs.loremipsum.LoremIpsum;
 import unipotsdam.gf.modules.fileManagement.FileRole;
+import unipotsdam.gf.modules.group.Group;
+import unipotsdam.gf.modules.group.GroupDAO;
 import unipotsdam.gf.modules.project.Project;
 import unipotsdam.gf.modules.reflection.model.LearningGoalRequest;
 import unipotsdam.gf.modules.reflection.model.LearningGoalStoreItem;
+import unipotsdam.gf.modules.reflection.model.ReflectionQuestion;
 import unipotsdam.gf.modules.reflection.model.ReflectionQuestionsStoreItem;
 import unipotsdam.gf.modules.reflection.service.LearningGoalStoreDAO;
 import unipotsdam.gf.modules.reflection.service.LearningGoalsDAO;
@@ -12,9 +15,12 @@ import unipotsdam.gf.modules.reflection.service.ReflectionQuestionDAO;
 import unipotsdam.gf.modules.reflection.service.ReflectionQuestionsStoreDAO;
 import unipotsdam.gf.modules.submission.controller.SubmissionController;
 import unipotsdam.gf.modules.submission.model.FullSubmission;
+import unipotsdam.gf.modules.submission.model.FullSubmissionPostRequest;
 import unipotsdam.gf.modules.submission.model.Visibility;
 import unipotsdam.gf.modules.user.User;
 import unipotsdam.gf.modules.user.UserDAO;
+import unipotsdam.gf.modules.wizard.compbase.TomcatConceptImporter;
+import unipotsdam.gf.process.DossierCreationProcess;
 import unipotsdam.gf.process.IExecutionProcess;
 import unipotsdam.gf.process.tasks.Progress;
 import unipotsdam.gf.process.tasks.Task;
@@ -22,13 +28,17 @@ import unipotsdam.gf.process.tasks.TaskDAO;
 import unipotsdam.gf.process.tasks.TaskName;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-public class ReflectionPhaseSimulation {
+import static unipotsdam.gf.modules.wizard.Wizard.convertTextToQuillJs;
 
+public class ReflectionPhaseSimulation {
+    
     @Inject
     private IExecutionProcess iExecutionProcess;
 
@@ -47,10 +57,24 @@ public class ReflectionPhaseSimulation {
     private SubmissionController submissionController;
 
     @Inject
+    private DossierCreationProcess dossierCreationProcess;
+
+    @Inject
     UserDAO userDAO;
 
     @Inject
     TaskDAO taskDAO;
+
+    @Inject
+    GroupDAO groupDAO;
+
+    private final TomcatConceptImporter concepts;
+    private LoremIpsum loremIpsum;
+
+    public ReflectionPhaseSimulation() throws UnsupportedEncodingException {
+        loremIpsum = new LoremIpsum();
+        this.concepts = new TomcatConceptImporter();
+    }
 
     public void simulateQuestionSelection(Project project) throws Exception {
 
@@ -65,7 +89,7 @@ public class ReflectionPhaseSimulation {
                 List<LearningGoalStoreItem> selectedLearningGoals = new ArrayList<>();
                 List<ReflectionQuestionsStoreItem> reflectionQuestionsStoreItems = new ArrayList<>();
                 Random random = new Random();
-                for (int i = 0; i< 10; i++) {
+                for (int i = 0; i < 10; i++) {
                     int y = random.nextInt(allStoreGoals.size());
                     LearningGoalStoreItem learningGoalStoreItem = allStoreGoals.get(y);
                     selectedLearningGoals.add(learningGoalStoreItem);
@@ -76,8 +100,8 @@ public class ReflectionPhaseSimulation {
                     // it should finalize with the last:
                     learningGoalRequest.setEndTask(i == 9);
                     for (int z = 0; z < 3; z++) {
-                        List<ReflectionQuestionsStoreItem> learningGoalSpecificQuestions =
-                                reflectionQuestionsStoreDAO.getLearningGoalSpecificQuestions(learningGoalStoreItem.getText());
+                        List<ReflectionQuestionsStoreItem> learningGoalSpecificQuestions = reflectionQuestionsStoreDAO
+                                .getLearningGoalSpecificQuestions(learningGoalStoreItem.getText());
                         int questionIndex = random.nextInt(2);
                         ReflectionQuestionsStoreItem reflectionQuestionsStoreItem =
                                 learningGoalSpecificQuestions.get(questionIndex);
@@ -91,9 +115,8 @@ public class ReflectionPhaseSimulation {
         }
     }
 
-
-    public void simulateCreatingPortfolioEntries(Project project) {
-        // todo implement
+    public void simulateCreatingPortfolioEntries(Project project) throws Exception {
+            simulateSubmissions(project, FileRole.PORTFOLIO_ENTRY, Visibility.GROUP);
     }
 
     public void simulateDocentFeedback(Project project) {
@@ -102,28 +125,55 @@ public class ReflectionPhaseSimulation {
     }
 
     public void simulateChooseingPortfolioEntries(Project project) throws Exception {
-        List<User> usersByProjectName = userDAO.getUsersByProjectName(project.getName());
-        for (User user : usersByProjectName) {
-
-            List<FullSubmission> personalSubmissions =
-                    submissionController.getPersonalSubmissions(user, project, FileRole.PORTFOLIO_ENTRY);
-
-            // TODO @Martin plz refactor this
-            List<FullSubmission> portfolioEntries = submissionController.getPersonalSubmissions(user, project, FileRole.PORTFOLIO_ENTRY);
-            List<FullSubmission> groupEntries = portfolioEntries.stream()
-                    .filter(entry -> entry.getVisibility() == Visibility.GROUP || entry.getVisibility() == Visibility.PUBLIC)
-                    .sorted().collect(Collectors.toList());
-            Random random = new Random();
-            int numberselected = random.nextInt(2);
-            List<FullSubmission> fullSubmissions = groupEntries.subList(0, numberselected);
-            iExecutionProcess.selectPortfolioEntries(project, user, fullSubmissions);
+        List<FullSubmission> selectedGroupPortfolioEntries =
+                submissionController.getSelectedGroupPortfolioEntries(project);
+        if (selectedGroupPortfolioEntries == null || selectedGroupPortfolioEntries.isEmpty()) {
+            List<User> usersByProjectName = userDAO.getUsersByProjectName(project.getName());
+            for (User user : usersByProjectName) {
+                // TODO @Martin plz refactor this
+                List<FullSubmission> portfolioEntries =
+                        submissionController.getPersonalSubmissions(user, project, FileRole.PORTFOLIO_ENTRY);
+                List<FullSubmission> groupEntries = portfolioEntries.stream()
+                        .filter(entry -> entry.getVisibility() == Visibility.GROUP
+                                || entry.getVisibility() == Visibility.PUBLIC).sorted().collect(Collectors.toList());
+                Random random = new Random();
+                int numberselected = random.nextInt(2);
+                List<FullSubmission> fullSubmissions = groupEntries.subList(0, numberselected);
+                iExecutionProcess.selectPortfolioEntries(project, user, fullSubmissions);
+            }
         }
-
-
     }
 
-    public void simulateAnsweringReflectiveQuestions(Project project) {
+    public void simulateAnsweringReflectiveQuestions(Project project) throws Exception {
+        FileRole rq = FileRole.REFLECTION_QUESTION;
+        simulateSubmissions(project, rq, Visibility.DOCENT);
+        
+    }
 
+    private void simulateSubmissions(Project project, FileRole rq, Visibility visibility) throws Exception {
+        List<FullSubmission> projectSubmissions =
+                submissionController.getProjectSubmissions(project, rq, visibility);
+        if (projectSubmissions == null || projectSubmissions.isEmpty()) {
+            List<User> usersByProjectName = userDAO.getUsersByProjectName(project.getName());
+            for (User user : usersByProjectName) {
+                List<ReflectionQuestion> reflectionQuestions =
+                        reflectionQuestionDAO.getUnansweredQuestions(project, user, false);
+                for (ReflectionQuestion reflectionQuestion : reflectionQuestions) {
+                    Group myGroup = groupDAO.getMyGroup(user, project);
+                    String text = loremIpsum.getWords(500);
+                    text = convertTextToQuillJs(text);
+                    String title = concepts.getNumberedConcepts(3).stream().reduce((x, y) -> x + " " + y).get();
+                    FullSubmissionPostRequest submission =
+                            new FullSubmissionPostRequest(myGroup, text, rq, project, visibility, title);
+                    submission.setHtml(text);
+                    FullSubmission fullSubmission = dossierCreationProcess.addDossier(submission, user, project);
+                    submission.setId(fullSubmission.getId());
+                    iExecutionProcess.answerReflectionQuestion(fullSubmission, reflectionQuestion);
+                    // dossier creation processs is used as substitute -- needs refactoring
+                    //dossierCreationProcess.addDossier(submission, user, project);
+                }
+            }
+        }
     }
 
 }
